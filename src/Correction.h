7f44aca57f948552bfc96198ce0c650b61dc9eac
abcdaaaaaaaaa <108553778+abcdaaaaaaaaa@users.noreply.github.com>
2025-07-01 18:36:43 +0300

    Update Correction.h

--- src/Correction.h
+++ src/Correction.h
@@ -11,3 +11,4 @@ float unsupported_calculateCorrection1(float temp, float rh, float a_RH33, float
 float unsupported_calculateCorrection2(float temp, float rh, float a_RH30, float b_RH30, float a_RH60, float b_RH60, float a_RH85, float b_RH85);
 
 #endif
+
