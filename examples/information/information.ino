045a32165ececcc38026e5bf0feeaafc1e9f8bd8
abcdaaaaaaaaa <108553778+abcdaaaaaaaaa@users.noreply.github.com>
2025-07-02 01:05:42 +0300

    Update information.ino

--- examples/information/information.ino
+++ examples/information/information.ino
@@ -1,1 +1,15 @@
-// Basic information about the library will be available here very soon!
+For data science applications: https://github.com/abcdaaaaaaaaa/MQDataScience/tree/main/DataScience
+For correction coefficient assisted ppm estimation in Data Science Applications: https://github.com/abcdaaaaaaaaa/MQDataScience/blob/main/DataScience/4D_Slope/4DSlope.py
+For model gases ppm estimation in Data Science Applications: https://github.com/abcdaaaaaaaaa/MQDataScience/blob/main/DataScience/Models/ModelGases/ModelSlope.py
+For 3D air quality graphical sensor simulation with correction factor support in data science applications: https://github.com/abcdaaaaaaaaa/MQDataScience/blob/main/DataScience/3D_Surface/3DSurface.py
+For air quality graphical sensor simulation of model gases in data science applications: https://github.com/abcdaaaaaaaaa/MQDataScience/blob/main/DataScience/Models/ModelGases/ModelGraph.py
+
+
+Correction Coefficient: Mathematical modeling of the compensation of incorrect ppm rate measured due to negative effects of environmental conditions based on temperature and humidity of the sensor.
+
+Standard Gases: Gases measured by correction coefficient supported "Standard Sensors".
+Different Model Gases: Gases measured by "Standard Sensors" without correction coefficient support.
+  
+
+
+
