c1e009c504cff22f9670c39a3e44915e33e8dec1
abcdaaaaaaaaa <108553778+abcdaaaaaaaaa@users.noreply.github.com>
2025-07-04 23:00:06 +0300

    Add files via upload

---
+++ examples/Required_MQ_LoadResistor/Required_MQ_LoadResistor.ino
@@ -0,0 +1,25 @@
+ // WARNING: Necessary for all users, Here are all the resistance values ​​you need to apply for supported sensors:
+ /* MQ135:     20  kΩ
+  * MQ2:       5   kΩ
+  * MQ3:       200 kΩ
+  * MQ4:       20  kΩ
+  * MQ5:       20  kΩ
+  * MQ6:       20  kΩ
+  * MQ7:       10  kΩ
+  * MQ8:       10  kΩ
+  * MQ9:       10  kΩ
+  * MQ131_LOW: 20  kΩ
+  * MQ131:     20  kΩ
+  * MQ136:     20  kΩ
+  * MQ137:     47  kΩ
+  * MQ138:     20  kΩ
+  * MQ214:     50  kΩ
+  * MQ303A:    47  kΩ
+  * MQ303B:    10  kΩ
+  * MQ307A:    1   kΩ
+  * MQ309A:    10  kΩ
+  */
+
+  /* The graph shows the recommended resistance values specified in each sensor's datasheet as the most accurate operating points.
+  *  Although the datasheets also indicate broader operational ranges beyond these ideal values,
+  *  the library enforces the use of only the optimal ones to ensure consistent measurement accuracy. */
