dfdc4b219a59a35022e285086dfd988a3f9213c6
abcdaaaaaaaaa <108553778+abcdaaaaaaaaa@users.noreply.github.com>
2025-07-17 17:35:29 +0300

    Update AirQuality.cpp

--- src/AirQuality.cpp
+++ src/AirQuality.cpp
@@ -18,11 +18,12 @@ float logarithmic_interpolate(float x, float x_min, float x_max, float ppm_min, 
 
 float airConcentration(String m, float sensorVal) {
   float ppm_min, ppm_max;
-  if (m == "MQ2" || m == "MQ4" || m == "MQ5" || m == "MQ6" || m == "MQ8" || m == "MQ9" || m == "MQ136" || m == "MQ137" || m == "MQ138") ppm_min = 200, ppm_max = 10000;
+  if (m == "MQ2" || m == "MQ4" || m == "MQ5" || m == "MQ6" || m == "MQ8" || m == "MQ9" || m == "MQ136" || m == "MQ137" || m == "MQ138" || m == "MQ216") ppm_min = 200, ppm_max = 10000;
   else if (m == "MQ303A" || m == "MQ303B"){ ppm_min = 1.2, ppm_max = 3.45; }
   else if (m == "MQ7"){ ppm_min = 50, ppm_max = 4000; }
   else if (m == "MQ135"){ ppm_min = 10, ppm_max = 200; }
   else if (m == "MQ214"){ ppm_min = 1000; ppm_max = 20000; }
+  else if (m == "MQ306A"){ ppm_min = 15; ppm_max = 30; }
   else if (m == "MQ309A"){ ppm_min = 160, ppm_max = 260; }
   else if (m == "MQ3"){ ppm_min = 0.103; ppm_max = 10; } // the result must be multiplied by ×50 to convert the mg/L --> ppm
   else if (m == "MQ131" || m == "MQ131_LOW"){ ppm_min = 5; ppm_max = 100; } // for MQ131_LOW the result must be multiplied by ×0.02 to convert the ppb --> ppm
