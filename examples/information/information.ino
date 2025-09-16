54098830f046ae32a3cdec469d6ee96407355612
abcdaaaaaaaaa <108553778+abcdaaaaaaaaa@users.noreply.github.com>
2025-09-16 23:04:08 +0300

    Update information.ino

--- examples/information/information.ino
+++ examples/information/information.ino
@@ -10,7 +10,7 @@
 ---------- For Data Science ----------
   For data science applications: https://github.com/abcdaaaaaaaaa/MQDataScience/tree/main/DataScience
   For correction coefficient assisted 4d ppm slope estimation in Data Science Applications: https://github.com/abcdaaaaaaaaa/MQDataScience/blob/main/DataScience/4D_Slope/4DSlope.py
-  For only read current ppm values with correction coefficient in Data Science Applications: https://github.com/abcdaaaaaaaaa/MQDataScience/blob/main/DataScience/4D_Slope/readppm.py
+  For only read and estimate current ppm values with correction coefficient (does not include 4D axis features) in Data Science Applications: https://github.com/abcdaaaaaaaaa/MQDataScience/blob/main/DataScience/4D_Slope/readppm.py
   For model gases ppm estimation in Data Science Applications: https://github.com/abcdaaaaaaaaa/MQDataScience/blob/main/DataScience/Models/ModelGases/ModelSlope.py
   For 3D air quality graphical sensor simulation with correction coefficient support in data science applications: https://github.com/abcdaaaaaaaaa/MQDataScience/blob/main/DataScience/3D_Surface/3DSurface.py
   For air quality graphical sensor simulation of model gases in data science applications: https://github.com/abcdaaaaaaaaa/MQDataScience/blob/main/DataScience/Models/ModelGases/ModelGraph.py
@@ -32,4 +32,3 @@
   To process supported MQ sensors to the microcontroller: All_Supported_MQ_Sensors.ino
   To obtain more concrete information about gas sensors: curious.ino
   To have general information about all the files in the library: information.ino
-
