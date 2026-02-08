834dc3b64ad429897e77871b584bc6904d64f39e
abcdaaaaaaaaa <108553778+abcdaaaaaaaaa@users.noreply.github.com>
2026-02-08 15:06:33 +0300

    Update voltage range comments for supported sensors

--- examples/Requireds/Required_MQ_VoltageRanges/Required_MQ_VoltageRanges.ino
+++ examples/Requireds/Required_MQ_VoltageRanges/Required_MQ_VoltageRanges.ino
@@ -1,26 +1,26 @@
   // WARNING: Necessary for all users, Here are all the voltage ranges ​​you need to apply for supported sensors:
 
- /* MQ135:     5.0V ±0.1
-  * MQ2:       5.0V ±0.1
-  * MQ3:       5.0V ±0.1
-  * MQ4:       5.0V ±0.1
-  * MQ5:       5.0V ±0.1
-  * MQ6:       5.0V ±0.1
-  * MQ7:       5.0V ±0.1
-  * MQ8:       5.0V ±0.1
-  * MQ9:       5.0V ±0.1
-  * MQ131_LOW: 5.0V ±0.1
-  * MQ131:     5.0V ±0.1
-  * MQ136:     5.0V ±0.1
-  * MQ137:     5.0V ±0.1
-  * MQ138:     5.0V ±0.1
-  * MQ214:     6.0V ±0.1
-  * MQ216:     6.0V ±0.1
+ /* MQ135:     5.0V ±0.1V
+  * MQ2:       5.0V ±0.1V
+  * MQ3:       5.0V ±0.1V
+  * MQ4:       5.0V ±0.1V
+  * MQ5:       5.0V ±0.1V
+  * MQ6:       5.0V ±0.1V
+  * MQ7:       5.0V ±0.1V
+  * MQ8:       5.0V ±0.1V
+  * MQ9:       5.0V ±0.1V
+  * MQ131_LOW: 5.0V ±0.1V
+  * MQ131:     5.0V ±0.1V
+  * MQ136:     5.0V ±0.1V
+  * MQ137:     5.0V ±0.1V
+  * MQ138:     5.0V ±0.1V
+  * MQ214:     6.0V ±0.1V
+  * MQ216:     6.0V ±0.1V
   * MQ303A:    ↓6.0V (Less than 6V) --> 5.0V is recommended.
   * MQ303B:    ↓6.0V (Less than 6V) --> 5.0V is recommended.
   * MQ306A:    ↓6.0V (Less than 6V) --> 5.0V is recommended.
-  * MQ307A:    6.0V ±0.1
-  * MQ309A:    6.0V ±0.1
+  * MQ307A:    6.0V ±0.1V
+  * MQ309A:    6.0V ±0.1V
   */
 
   /* The graph shows the required voltage ranges specified in each sensor's datasheet as the most accurate operating points.
@@ -28,3 +28,4 @@
   *  Since the tolerance amount is not specified in the datasheet for some sensors, the tolerance amount for those sensors is not shown here either. */
 
   // NOTE: These are the voltages that need to be sent to the sensor, both for preheating and for standard ppm readings.
+
