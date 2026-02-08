0d6d39f5d0b89738afaa41126f889a97372cba0b
abcdaaaaaaaaa <108553778+abcdaaaaaaaaa@users.noreply.github.com>
2026-02-08 13:50:59 +0300

    Add files via upload

---
+++ examples/Requireds/Required_MQ_PreheatingTimes/Required_MQ_PreheatingTimes.ino
@@ -0,0 +1,31 @@
+  // WARNING: Necessary for all users, Here are all the preheating times ​​you need to apply for supported sensors:
+
+ /* MQ135:     ↑24h
+  * MQ2:       ↑24h
+  * MQ3:       ↑24h
+  * MQ4:       ↑24h
+  * MQ5:       ↑24h
+  * MQ6:       ↑24h
+  * MQ7:       ↑48h
+  * MQ8:       ↑24h
+  * MQ9:       ↑48h
+  * MQ131_LOW: ↑24h
+  * MQ131:     ↑48h
+  * MQ136:     ↑24h
+  * MQ137:     ↑24h
+  * MQ138:     ↑24h
+  * MQ214:     ↑24h
+  * MQ216:     ↑24h
+  * MQ303A:    ↓48h
+  * MQ303B:    ↑24h
+  * MQ306A:    ↑24h
+  * MQ307A:    ↑24h
+  * MQ309A:    ↑24h
+  */
+
+  /* The graph shows the required preheating times specified in each sensor's datasheet as the most accurate operating points.
+  *  The (↓) down arrow indicates that the ideal preheating time is less than that time, and the (↑) up arrow indicates that the ideal preheating time is more than that time. */
+
+  // NOTES: Gas sensors must be preheated for a certain period before initial use. Failure to do so will result in unstable sensor outputs.
+  // Preheating stabilizes the sensor's operating conditions, ensuring reliable and repeatable measurements.
+  // This idle time of the sensor must be implemented with the appropriate resistor and voltage specified in the Requires folder.
---
+++ examples/Requireds/Required_MQ_VoltageRanges/Required_MQ_VoltageRanges.ino
@@ -0,0 +1,30 @@
+  // WARNING: Necessary for all users, Here are all the voltage ranges ​​you need to apply for supported sensors:
+
+ /* MQ135:     5.0V ±0.1
+  * MQ2:       5.0V ±0.1
+  * MQ3:       5.0V ±0.1
+  * MQ4:       5.0V ±0.1
+  * MQ5:       5.0V ±0.1
+  * MQ6:       5.0V ±0.1
+  * MQ7:       5.0V ±0.1
+  * MQ8:       5.0V ±0.1
+  * MQ9:       5.0V ±0.1
+  * MQ131_LOW: 5.0V ±0.1
+  * MQ131:     5.0V ±0.1
+  * MQ136:     5.0V ±0.1
+  * MQ137:     5.0V ±0.1
+  * MQ138:     5.0V ±0.1
+  * MQ214:     6.0V ±0.1
+  * MQ216:     6.0V ±0.1
+  * MQ303A:    ↓6.0V (Less than 6V) --> 5.0V is recommended.
+  * MQ303B:    ↓6.0V (Less than 6V) --> 5.0V is recommended.
+  * MQ306A:    ↓6.0V (Less than 6V) --> 5.0V is recommended.
+  * MQ307A:    6.0V ±0.1
+  * MQ309A:    6.0V ±0.1
+  */
+
+  /* The graph shows the required voltage ranges specified in each sensor's datasheet as the most accurate operating points.
+  *  The ± symbol indicates the maximum voltage value that the sensor can tolerate above its ideal voltage value.
+  *  Since the tolerance amount is not specified in the datasheet for some sensors, the tolerance amount for those sensors is not shown here either. */
+
+  // NOTE: These are the voltages that need to be sent to the sensor, both for preheating and for standard ppm readings.
