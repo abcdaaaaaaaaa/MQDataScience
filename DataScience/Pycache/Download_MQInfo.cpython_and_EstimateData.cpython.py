6a8918ba80ec1e630bb3e0e046414108ba65679f
abcdaaaaaaaaa <108553778+abcdaaaaaaaaa@users.noreply.github.com>
2026-06-12 21:11:02 +0300

    Add files via upload

---
+++ DataScience/Pycache/Download_MQInfo.cpython_and_EstimateData.cpython.py
@@ -0,0 +1,4 @@
+# After running this code, the system will create the file MQInfo.cpython.py from MQInfo.py.
+# After running this code, the system will create the file MQInfo.cpython.py from EstimateData.py.
+import MQInfo
+import EstimateData
