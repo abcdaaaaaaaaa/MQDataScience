1273bd5e014afab002a68da6a05dfd0a65628f06
abcdaaaaaaaaa <108553778+abcdaaaaaaaaa@users.noreply.github.com>
2026-06-21 22:56:43 +0300

    Update data.js

--- DataScript/data.js
+++ DataScript/data.js
@@ -1,8 +1,8 @@
 import * as MQInfo from "./MQInfo.js";
 
-let f, standard, Air, scaledTemperature, correction_coefficient, air, minair, maxair, lastppmvalue;
+let f, standard, Air, correction_coefficient, air, minair, maxair, lastppmvalue;
 
-switch (data1) {
+switch (toplam1) {
     case 1: f = "MQ135"; break;
     case 2: f = "MQ2"; break;
     case 3: f = "MQ3"; break;
@@ -26,7 +26,7 @@ switch (data1) {
     case 21: f = "MQ309A"; break;
 }
 
-switch (data1) { 
+switch (toplam1) { 
     case 1: case 2: case 3: case 4: case 5: case 6: case 7: case 8: case 9: case 10: case 12: case 13: case 14: case 15: case 16: standard = 1; break; 
     default: standard = 0; break; 
 }
@@ -40,16 +40,20 @@ const MinAirPpm = MQInfo.MinAirPpm;
 const MaxAirPpm = MQInfo.MaxAirPpm;
 const gas_params = MQInfo.gas_params;
 
-let a_RH30, b_RH30, a_RH33, b_RH33, a_RH60, b_RH60, a_RH85, b_RH85;
+let a_RH30, b_RH30, c_RH30, a_RH33, b_RH33, c_RH33, a_RH60, b_RH60, c_RH60, a_RH85, b_RH85, c_RH85;
 
 try { a_RH30 = MQInfo.a_RH30; } catch (e) {}
 try { b_RH30 = MQInfo.b_RH30; } catch (e) {}
+try { c_RH30 = MQInfo.c_RH30; } catch (e) {}
 try { a_RH33 = MQInfo.a_RH33; } catch (e) {}
 try { b_RH33 = MQInfo.b_RH33; } catch (e) {}
+try { c_RH33 = MQInfo.c_RH33; } catch (e) {}
 try { a_RH60 = MQInfo.a_RH60; } catch (e) {}
 try { b_RH60 = MQInfo.b_RH60; } catch (e) {}
+try { c_RH60 = MQInfo.c_RH60; } catch (e) {}
 try { a_RH85 = MQInfo.a_RH85; } catch (e) {}
 try { b_RH85 = MQInfo.b_RH85; } catch (e) {}
+try { c_RH85 = MQInfo.c_RH85; } catch (e) {}
 
 const SensorRLCalRL = MQInfo.SensorRLCalRL;
 const SensorMode = MQInfo.SensorMode;
@@ -69,10 +73,6 @@ function round4(value) {
     return Math.round(value * Math.pow(10, 4)) / Math.pow(10, 4);
 }
 
-function yaxb(valuea, value, valueb) {
-    return valuea * Math.pow(value, valueb);
-}
-
 function inverseyaxb(valuea, value, valueb) {
     return Math.pow(value / valuea, 1 / valueb);
 }
@@ -89,33 +89,25 @@ function exponentialInterpolate(value, minValue, maxValue, targetMin, targetMax)
     return Math.pow(10, logVal);
 }
 
-function ScaleTemp(temp, mode) {
-    let temp_scaled;
-    if (mode === '+') {
-        temp_scaled = (temp + 25) / 15;
-        if (CRMode !== 1) temp_scaled = (temp + 15) / 5;
-    } else if (mode === '-') {
-        temp_scaled = 15 * temp - 25;
-        if (CRMode !== 1) temp_scaled = 5 * temp - 15;
-    }
-    return temp_scaled;
-}
-
 function CorrectionCoefficient(temp, RH) {
-    let TH_valuea, TH_valueb;
-    if (CRMode !== 3) {
-        TH_valuea = interpolate(RH, 33, 85, a_RH33, a_RH85);
-        TH_valueb = interpolate(RH, 33, 85, b_RH33, b_RH85);
-    } else {
-        if (RH <= 60) {
-	  TH_valuea = interpolate(RH, 30, 60, a_RH30, a_RH60);
-	  TH_valueb = interpolate(RH, 30, 60, b_RH30, b_RH60);
-        } else {
-	  TH_valuea = interpolate(RH, 60, 85, a_RH60, a_RH85);
-	  TH_valueb = interpolate(RH, 60, 85, b_RH60, b_RH85);
-        }
-    }
-    return yaxb(TH_valuea, temp, TH_valueb);
+    let TH_valuea, TH_valueb, TH_valuec;
+	if (CRMode == 3) {
+		if (RH <= 60) {
+			TH_valuea = interpolate(RH, 30, 60, a_RH30, a_RH60);
+			TH_valueb = interpolate(RH, 30, 60, b_RH30, b_RH60);
+			TH_valuec = interpolate(RH, 30, 60, c_RH30, c_RH60);
+		} else {
+			TH_valuea = interpolate(RH, 60, 85, a_RH60, a_RH85);
+			TH_valueb = interpolate(RH, 60, 85, b_RH60, b_RH85);
+			TH_valuec = interpolate(RH, 60, 85, c_RH60, c_RH85);
+		}
+	} else {
+		TH_valuea = interpolate(RH, 33, 85, a_RH33, a_RH85);
+		TH_valueb = interpolate(RH, 33, 85, b_RH33, b_RH85);
+		TH_valuec = interpolate(RH, 33, 85, c_RH33, c_RH85);
+	}
+
+	return TH_valuea + TH_valuec * Math.exp(TH_valueb * temp);
 }
 
 function limit(value, minlim, maxlim) {
@@ -137,17 +129,16 @@ function Sensorppm2(valuea, valueb, SensorValue) {
     return convertppm(inverseyaxb(valuea, SensorRatio_value, valueb));
 }
 
-const SensorValue = round4(limit((data2 / (Math.pow(2, 12) - 1)), 0, 1)); // analogRead()
-const temperature = round4(limit(((data3 / 10) - 140), -10, 50)); // The IOT system you are using may be having problems with negative numbers.
-const rh = round4(limit(((data4 / 10) - 100), 0, 100)); // The IOT system you are using may be having problems with negative numbers.
-const SensorValue2 = round4(limit((data5 / (Math.pow(2, 12) - 1)), 0, 1)); // analogRead()
+const SensorValue = round4(limit((toplam2 / (Math.pow(2, 12) - 1)), 0, 1)); // analogRead()
+const temperature = round4(limit(((toplam3 / 10) - 140), -10, 50)); // The IOT system you are using may be having problems with negative numbers.
+const rh = round4(limit(((toplam4 / 10) - 100), 0, 100)); // The IOT system you are using may be having problems with negative numbers.
+const SensorValue2 = round4(limit((toplam5 / (Math.pow(2, 12) - 1)), 0, 1)); // analogRead()
 
 const percentile = SensorValue * 100;
 const percentile2 = SensorValue2 * 100;
 
 if (standard == 1) {
-    scaledTemperature = ScaleTemp(temperature, '+');
-    correction_coefficient = CorrectionCoefficient(scaledTemperature, rh);
+    correction_coefficient = CorrectionCoefficient(temperature, rh);
     air = Math.round(limit(exponentialInterpolate(SensorValue, 0, 1, convertppm(MinAirPpm), convertppm(MaxAirPpm)), 1, convertppm(MaxAirPpm)) * correction_coefficient);
 } else {
     if (f == "MQ131_LOW") { 
@@ -155,11 +146,11 @@ if (standard == 1) {
     } else { air = Math.round(limit(exponentialInterpolate(SensorValue, 0, 1, convertppm(MinAirPpm), convertppm(MaxAirPpm)), 1, convertppm(MaxAirPpm))) }
 }
 
-document.cookie = "value1="+percentile;
-document.cookie = "value2="+air;
-document.cookie = "value3="+temperature;
-document.cookie = "value4="+rh;
-document.cookie = "value5="+percentile2;
+document.cookie = "value2="+percentile;
+document.cookie = "value3="+air;
+document.cookie = "value11="+temperature;
+document.cookie = "value12="+rh;
+document.cookie = "value13="+percentile2;
 
 gas_params.forEach((gas, i) => {
     
@@ -185,7 +176,7 @@ gas_params.forEach((gas, i) => {
         globalThis.CalValue = limit(interpolate(calAir, minair, maxair, 0, 1), 0.01, 0.99);
         minair = convertppm(minair)
         maxair = convertppm(maxair);
-        lastppmvalue = Math.round(limit(Sensorppm(adjusted_valuea, adjusted_valueb, SensorValue, CorrectionCoefficient(scaledTemperature, rh)), 1, maxair * CorrectionCoefficient(scaledTemperature, rh)));
+        lastppmvalue = Math.round(limit(Sensorppm(adjusted_valuea, adjusted_valueb, SensorValue, CorrectionCoefficient(temperature, rh)), 1, maxair * CorrectionCoefficient(temperature, rh)));
     } else {
         globalThis.CalValue = gas.calvalue ?? interpolate(calAir, minair, maxair, 0, 1);
         minair = convertppm(minair)
@@ -194,8 +185,7 @@ gas_params.forEach((gas, i) => {
             lastppmvalue = round2(limit(Sensorppm2(adjusted_valuea, adjusted_valueb, SensorValue), 0.01, maxair));
         } else { lastppmvalue = Math.round(limit(Sensorppm2(adjusted_valuea, adjusted_valueb, SensorValue), 1, maxair)) }
     }
-    
     globalThis[`ppmvalue${i+1}`] = lastppmvalue;
-    document.cookie = `value${i+6}=${globalThis[`ppmvalue${i+1}`]}`;
+    document.cookie = `value${i+4}=${globalThis[`ppmvalue${i+1}`]}`;
 
 });
