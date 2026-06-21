edee5eb6da6b801dfbfdadd0d11e59713dd9e338
abcdaaaaaaaaa <108553778+abcdaaaaaaaaa@users.noreply.github.com>
2026-06-21 22:18:10 +0300

    Update Correction.cpp

--- src/Correction.cpp
+++ src/Correction.cpp
@@ -11,111 +11,106 @@ float limit(float value, float minVal, float maxVal) {
     return value;
 }
 
-float scaleTemperature(float temp, int mode) {
-    switch(mode) {
-	case 1: return (temp + 25.0f) / 15.0f;
-	default: return (temp + 15.0f) / 5.0f;
-    }
-}
-
 float calculateCorrection(float temp, float rh, const String& model) {
-    float rh1 = limit(rh, 33, 85);
-    float rh2 = limit(rh, 30, 85);
-    float temp1 = limit(temp, -10, 50);
-  	
-    float t1 = scaleTemperature(temp1, 1);
-    float t2 = scaleTemperature(temp1, 2);
-    float t3 = scaleTemperature(temp1, 3);
+    float temp1 = limit(temp, -10.0f, 50.0f);
+    
+    // Group 3: CRMode == 3 (MQ9, MQ131)
+    if (model == "MQ131" || model == "MQ9") {
+        float rh2 = limit(rh, 30.0f, 85.0f);
+        float a, b, c;
+        if (rh2 <= 60.0f) {
+            a = fmap(rh2, 30.0f, 60.0f, -2.5404f, -1.6874f);
+            b = fmap(rh2, 30.0f, 60.0f, -0.0037f, -0.0043f);
+            c = fmap(rh2, 30.0f, 60.0f, 4.1019f, 3.0105f);
+        } else {
+            a = fmap(rh2, 60.0f, 85.0f, -1.6874f, -1.2399f);
+            b = fmap(rh2, 60.0f, 85.0f, -0.0043f, -0.0047f);
+            c = fmap(rh2, 60.0f, 85.0f, 3.0105f, 2.3914f);
+        }
+        return a + c * exp(b * temp1);
+    }
 
-    if (model == "MQ2" || model == "MQ135" || model == "MQ138" || model == "MQ214" || model == "MQ216") {		
-        float a = fmap(rh1, 33, 85, 1.6867, 1.5291);
-        float b = fmap(rh1, 33, 85, -0.4263, -0.422);
-        return a * pow(t1, b);
+    // Standard Group: 33 to 85 RH range
+    float rh1 = limit(rh, 33.0f, 85.0f);
+    float a, b, c;
+
+    if (model == "MQ2" || model == "MQ135" || model == "MQ136" || model == "MQ137" || model == "MQ138" || model == "MQ214" || model == "MQ216") {
+        a = fmap(rh1, 33.0f, 85.0f, 0.8579f, 0.7818f);
+        b = fmap(rh1, 33.0f, 85.0f, -0.0543f, -0.0554f);
+        c = fmap(rh1, 33.0f, 85.0f, 0.4912f, 0.4378f);
+        return a + c * exp(b * temp1);
+    }
+    if (model == "MQ3") {
+        a = fmap(rh1, 33.0f, 85.0f, 0.7897f, 0.7319f);
+        b = fmap(rh1, 33.0f, 85.0f, -0.0423f, -0.0446f);
+        c = fmap(rh1, 33.0f, 85.0f, 0.5355f, 0.4069f);
+        return a + c * exp(b * temp1);
     }
     if (model == "MQ4") {
-        float a = fmap(rh1, 33, 85, 1.2767, 1.1013);
-        float b = fmap(rh1, 33, 85, -0.2204, -0.2482);
-        return a * pow(t1, b);
+        a = fmap(rh1, 33.0f, 85.0f, 0.8597f, 0.5838f);
+        b = fmap(rh1, 33.0f, 85.0f, -0.0381f, -0.0218f);
+        c = fmap(rh1, 33.0f, 85.0f, 0.2861f, 0.4064f);
+        return a + c * exp(b * temp1);
     }
     if (model == "MQ5") {
-        float a = fmap(rh1, 33, 85, 1.3645, 1.1315);
-        float b = fmap(rh1, 33, 85, -0.2977, -0.2874);
-        return a * pow(t1, b);
+        a = fmap(rh1, 33.0f, 85.0f, 0.8098f, 0.6066f);
+        b = fmap(rh1, 33.0f, 85.0f, -0.0413f, -0.0283f);
+        c = fmap(rh1, 33.0f, 85.0f, 0.3686f, 0.3891f);
+        return a + c * exp(b * temp1);
     }
     if (model == "MQ6") {
-        float a = fmap(rh1, 33, 85, 1.3128, 1.1252);
-        float b = fmap(rh1, 33, 85, -0.2428, -0.2496);
-        return a * pow(t1, b);
+        a = fmap(rh1, 33.0f, 85.0f, 0.8714f, 0.7287f);
+        b = fmap(rh1, 33.0f, 85.0f, -0.044f, -0.0412f);
+        c = fmap(rh1, 33.0f, 85.0f, 0.2883f, 0.2648f);
+        return a + c * exp(b * temp1);
     }
     if (model == "MQ7") {
-        float a = fmap(rh1, 33, 85, 1.4296, 1.1733);
-        float b = fmap(rh1, 33, 85, -0.3238, -0.2842);
-        return a * pow(t1, b);
+        a = fmap(rh1, 33.0f, 85.0f, 0.8315f, 0.6708f);
+        b = fmap(rh1, 33.0f, 85.0f, -0.0462f, -0.033f);
+        c = fmap(rh1, 33.0f, 85.0f, 0.3813f, 0.358f);
+        return a + c * exp(b * temp1);
     }
     if (model == "MQ8") {
-        float a = fmap(rh1, 33, 85, 1.1474, 1.0783);
-        float b = fmap(rh1, 33, 85, -0.1958, -0.1824);
-        return a * pow(t1, b);
+        a = fmap(rh1, 33.0f, 85.0f, 0.8559f, 0.8201f);
+        b = fmap(rh1, 33.0f, 85.0f, -0.0611f, -0.0606f);
+        c = fmap(rh1, 33.0f, 85.0f, 0.1673f, 0.1492f);
+        return a + c * exp(b * temp1);
     }
     /* if (model == "MQ9") {
-        float a = fmap(rh1, 33, 85, 1.4292, 1.1735);
-        float b = fmap(rh1, 33, 85, -0.3206, -0.2801);
-        return a * pow(t1, b);
+        a = fmap(rh1, 33.0f, 85.0f, 0.839f, 0.6884f);
+        b = fmap(rh1, 33.0f, 85.0f, -0.0469f, -0.0349f);
+        c = fmap(rh1, 33.0f, 85.0f, 0.3742f, 0.3403f);
+        return a + c * exp(b * temp1);
     } */
-    if (model == "MQ136" || model == "MQ137") {
-        float a = fmap(rh1, 33, 85, 1.6867, 1.5291);
-        float b = fmap(rh1, 33, 85, -0.4263, -0.422);
-        return a * pow(t1, b);
-    }
-  	if (model == "MQ3") {
-  	  float a = fmap(rh1, 33, 85, 1.6671, 1.4112);
-  	  float b = fmap(rh1, 33, 85, -0.2467, -0.2257);
-  	  return a * pow(t2, b);
-  	}
-  	if (model == "MQ131" || model == "MQ9") {
-  	   if (rh2 <= 60) {
-             float a = fmap(rh2, 30, 60, 1.876, 1.5885);
-             float b = fmap(rh2, 30, 60, -0.2284, -0.2271);
-             return a * pow(t3, b);
-  	} else {
-  	     float a = fmap(rh2, 60, 85, 1.5885, 1.3842);
-  	     float b = fmap(rh2, 60, 85, -0.2271, -0.2282);
-             return a * pow(t3, b);
-  	  }
-  	}
-    return 1.0;
+
+    return 1.0f;
 }
 
-float unsupported_calculateCorrection1(float temp, float rh, float a_RH33, float b_RH33, float a_RH85, float b_RH85, int scale_mode) {
-    float rh1 = limit(rh, 33, 85);
-    float temp1 = limit(temp, -10, 50);
+float unsupported_calculateCorrection1(float temp, float rh, float a_RH33, float b_RH33, float c_RH33, float a_RH85, float b_RH85, float c_RH85) {
+    float rh1 = limit(rh, 33.0f, 85.0f);
+    float temp1 = limit(temp, -10.0f, 50.0f);
     
-    float t1 = scaleTemperature(temp1, 1);
-    float t2 = scaleTemperature(temp1, 2);
+    float a = fmap(rh1, 33.0f, 85.0f, a_RH33, a_RH85);
+    float b = fmap(rh1, 33.0f, 85.0f, b_RH33, b_RH85);
+    float c = fmap(rh1, 33.0f, 85.0f, c_RH33, c_RH85);
 
-    float a = fmap(rh1, 33, 85, a_RH33, a_RH85);
-    float b = fmap(rh1, 33, 85, b_RH33, b_RH85);
-
-    if (scale_mode == 1) return a * pow(t1, b);
-    if (scale_mode == 2) return a * pow(t2, b);
-    
-    return 1.0;
+    return a + c * exp(b * temp1);
 }
 
-float unsupported_calculateCorrection2(float temp, float rh, float a_RH30, float b_RH30, float a_RH60, float b_RH60, float a_RH85, float b_RH85) {
-    float rh2 = limit(rh, 30, 85);
-    float temp1 = limit(temp, -10, 50);
+float unsupported_calculateCorrection2(float temp, float rh, float a_RH30, float b_RH30, float c_RH30, float a_RH60, float b_RH60, float c_RH60, float a_RH85, float b_RH85, float c_RH85) {
+    float rh2 = limit(rh, 30.0f, 85.0f);
+    float temp1 = limit(temp, -10.0f, 50.0f);
     
-    float t3 = scaleTemperature(temp1, 3);
+    float a, b, c;
 
-    if (rh2 <= 60) {
-      float a = fmap(rh2, 30, 60, a_RH30, a_RH60);
-      float b = fmap(rh2, 30, 60, b_RH30, b_RH60);
-      return a * pow(t3, b);
+    if (rh2 <= 60.0f) {
+        a = fmap(rh2, 30.0f, 60.0f, a_RH30, a_RH60);
+        b = fmap(rh2, 30.0f, 60.0f, b_RH30, b_RH60);
+        c = fmap(rh2, 30.0f, 60.0f, c_RH30, c_RH60);
     } else {
-      float a = fmap(rh2, 60, 85, a_RH60, a_RH85);
-      float b = fmap(rh2, 60, 85, b_RH60, b_RH85);
-      return a * pow(t3, b);
+        a = fmap(rh2, 60.0f, 85.0f, a_RH60, a_RH85);
+        b = fmap(rh2, 60.0f, 85.0f, b_RH60, b_RH85);
+        c = fmap(rh2, 60.0f, 85.0f, c_RH60, c_RH85);
     }
-    return 1.0;
+    return a + c * exp(b * temp1);
 }
