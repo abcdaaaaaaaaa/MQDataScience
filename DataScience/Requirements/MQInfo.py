b4b30df8b14927990e1cee4ce6d91187fbee6412
abcdaaaaaaaaa <108553778+abcdaaaaaaaaa@users.noreply.github.com>
2026-09-17 16:30:15 +0300

    Update MQInfo.py

--- DataScience/Requirements/MQInfo.py
+++ DataScience/Requirements/MQInfo.py
@@ -376,7 +376,7 @@ def MQ306A():
     SensorRLCalRL = 0.2
     MinAirPpm, MaxAirPpm = 15, 30
     gas_params = [
-        {'name': 'Ethanol', 'ppmvals': (100, 3000), 'ppm': (3.3559, -0.3563), 'calvalue': 0.873876},
+        {'name': 'Ethanol', 'ppmvals': (100, 3000), 'ppm': (3.3559, -0.3563), 'calvalue': 0.976666},
         {'name': 'Hydrogen', 'ppmvals': (100, 10000), 'ppm': (3.6812, -0.4308)},
         {'name': 'Methane', 'ppmvals': (100, 10000), 'ppm': (7.3489, -0.5979)},
         {'name': 'IsoButane', 'ppmvals': (100, 10000), 'ppm': (2.7521, -0.4732)}
@@ -390,7 +390,7 @@ def MQ307A():
     SensorRLCalRL = 0.02
     gas_params = [
         {'name': 'CO', 'ppmvals': (30, 1000), 'ppm': (2336.2698, -1.7255)},
-        {'name': 'H2', 'ppmvals': (1000, 3000), 'ppm': (25051.8177, -0.8318), 'calvalue': 0.999619}
+        {'name': 'H2', 'ppmvals': (1000, 3000), 'ppm': (25051.8177, -0.8318), 'calvalue': 0.999572}
     ]
     group2_subgroup4()
 
@@ -403,8 +403,8 @@ def MQ309A():
     gas_params = [
         {'name': 'H2', 'ppmvals': (10, 10000), 'ppm': (44.8077, -0.5156)},
         {'name': 'CH4', 'ppmvals': (10, 10000), 'ppm': (67.5665, -0.6088)},
-        {'name': 'alcohol', 'ppmvals': (100, 10000), 'ppm': (34.1956, -0.2666), 'calvalue': 0.83393},
-        {'name': 'CO', 'ppmvals': (100, 10000), 'ppm': (30.3019, -0.2336), 'calvalue': 0.83393}
+        {'name': 'alcohol', 'ppmvals': (100, 10000), 'ppm': (34.1956, -0.2666), 'calvalue': 0.988237},
+        {'name': 'CO', 'ppmvals': (100, 10000), 'ppm': (30.3019, -0.2336), 'calvalue': 0.988237}
     ]
     group2_subgroup2()
 
