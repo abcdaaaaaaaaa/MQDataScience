e3252956a45d04a29456dfe4ef5e20addd82d96f
abcdaaaaaaaaa <108553778+abcdaaaaaaaaa@users.noreply.github.com>
2026-09-17 17:36:26 +0300

    Update AirQuality.h

--- src/AirQuality.h
+++ src/AirQuality.h
@@ -4,7 +4,7 @@
 #include <Arduino.h>
 
 float exponential_interpolate(float x, float x_min, float x_max, float ppm_min, float ppm_max);
-float logarithmic_interpolate(float x, float x_min, float x_max, float ppm_min, float ppm_max);
+// float logarithmic_interpolate(float x, float x_min, float x_max, float ppm_min, float ppm_max);
 float airConcentration(String m, float sensorVal);
 float unsupported_airConcentration(float ppm_min, float ppm_max, float sensorVal);
 
