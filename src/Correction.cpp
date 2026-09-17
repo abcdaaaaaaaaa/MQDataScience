9966804cf4e355d9485c2dd9bb6617323500180d
abcdaaaaaaaaa <108553778+abcdaaaaaaaaa@users.noreply.github.com>
2026-09-17 17:36:41 +0300

    Update Correction.cpp

--- src/Correction.cpp
+++ src/Correction.cpp
@@ -1,5 +1,5 @@
 #include "Correction.h"
-#include <math.h>
+// #include <math.h>
 
 float fmap(float x, float in_min, float in_max, float out_min, float out_max) {
     return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min; // Arduino ide's map function does not support float structure.
